82f3f6a19b3864e1b71dc126bd0cb1ff72ebe5ff
Edgar Ortiz Lenovo <ed.ortizm@gmail.com>
2022-06-06 18:38:50 -0400

    feature: add new arguments to remove star option
    User can decide threshold for images, radius of pixels to set to zero and the fwhm for the gaussian kernel

--- src/leosAi/improcess/background.py
+++ src/leosAi/improcess/background.py
@@ -3,10 +3,13 @@
 from astropy.stats import sigma_clipped_stats
 import numpy as np
 from photutils.detection import DAOStarFinder
-from photutils.aperture import CircularAperture
 
 
-def remove_stars(image: np.array)->: np.array:
+def remove_stars(
+    image: np.array,
+    fwhm: float=3.,
+    threshold: int=5,
+    radius: int=5)-> np.array:
     """
     Remove stars from background but keeps satellite traces
     if present. For more info check:
@@ -14,24 +17,33 @@ def remove_stars(image: np.array)->: np.array:
 
     INPUTS
     image: array with observation image in magnitude units
+    fwhm: full width at half maximun for gaussian kernel to use
+        in the point spread function of a star
+    threshold: number of times the standard deviation of pixels values
+        in the image to gauge stars. Pixel values over it are cadidates
+        to be stars
+    radius: number of pixels from star's centroid to consider
+        to set as zero
 
     OUTPUT
+    positions: array with stars' positions in pixels' space
     image_no_stars: array with most of background stars removed
     """
 
-    mean, median, std = sigma_clipped_stats(image, sigma=3.0)
+    _, median, std = sigma_clipped_stats(image, sigma=3.0)
 
-    daofind = DAOStarFinder(fwhm=3.0, threshold=5.*std)
+    daofind = DAOStarFinder(fwhm=fwhm, threshold=threshold*std)
 
     sources = daofind(image - median)
     positions = np.transpose((sources['xcentroid'], sources['ycentroid']))
 
-    apertures = CircularAperture(positions, r=4.)
     positions = positions.astype(int)
 
     image_no_stars = image.copy()
 
     for pos in positions:
-        image_no_stars[pos[1]-3:pos[1]+3, pos[0]-3:pos[0]+3] = 0.
+        image_no_stars[
+            pos[1] - radius : pos[1] + radius, pos[0] - radius : pos[0] + 3
+        ] = 0.
 
-    return image_no_stars
+    return positions, image_no_stars
